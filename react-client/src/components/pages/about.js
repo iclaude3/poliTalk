447f230114695d774ec50a8c38458b52c0766bb1
Allina <allina606@gmail.com>
2019-05-12 14:39:04 -0400

    about page, bills API displaying results

---
+++ react-client/src/components/pages/about.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class About extends Component {
+    render() {
+        return(
+            <div className="container">
+            <div className="row">
+                <div className="bigText2">
+                    <p>Goal of PolyTalk</p> 
+                </div>
+
+                <div className="billCol">
+                    <p>Our goal as a team is to get more people involved in politics. 
+                        We want everyone to know who their representatives are in the area,
+                        what these positions mean, and how these representatives are impacting
+                        their area. 
+                    </p>
+                    <br />
+                    <p>
+                        <b>Where this information comes from: </b>
+                        We want to give users accurate information they can trust, which is why
+                        we provide all information about bills directly from congress.
+                    </p>
+                    <br />
+                </div>
+
+                <div className="bigText2">
+                    <p>About the Team</p> 
+                </div>
+
+                <div className="billCol">
+                    <p> 
+                       <b>Claudia Bandali and Allina Khan</b> 
+                    </p>
+                       
+                    <p>
+                        Claudia and Allina worked on getting your representatives and bills to you 
+                        and the design so that you can navigate seemlessly. If you come across any 
+                        problems, feel free to shoot us a message in our contact page! <br />
+                    </p>
+                    <p>
+                        Claudia is a software engineer at Goldman Sachs, and Allina is a software
+                        engineer intern at Visa Inc. Both are passionate about social justice and politics.
+                    </p>
+                    <br />
+                    <p>
+                        <b><i>Previous Contributors: </i></b> <br />
+                        <b>Estefenia Barron and Nouraiz Imran</b> 
+                    </p>
+                    <p>
+                        Nouraiz Imran goes to college of Staten Island and worked on getting your <br />
+                        representatives to you.
+                        <br />
+                    </p>
+                    <p>
+                        Big thanks to Estefenia Barron for her idea that got this app up and running!
+                        She is a computer science and journalism student at Brooklyn college. 
+                        She's made great contributions to her community! 
+                    </p>
+                    <br />               
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default About;
--- react-client/src/components/pages/bills.js
+++ react-client/src/components/pages/bills.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { BILLS_KEY } from "./keys.js";
+import 'tachyons';
+
+// billInfo pushes to dataRender, dataRender calls Title and holds all "titles" objs, then dataRender called to be displayed in render()
+// Title variable is what's displayed in webpage
+const Title = (props) => {
+    return (
+        <div className="billCol">
+            <p>{props.idx + ") " + props.number + ": " + props.title}</p>
+            <p>{props.desc}</p>
+            <br />
+        </div>
+    );
+}
 
 class bills extends Component {
     constructor() {
@@ -40,48 +53,55 @@ class bills extends Component {
     }
 
     render() {
+        //will include in program the values we want to print onto website 
         var billInfo = []; 
+        var dataRender = []; 
 
+        //getting values from state, and putting them in billInfo
         if (this.state.data.results != null) {
-            //console.log(this.state.data.results[0].bills); 
-            console.log(this.state.data.results[0].bills.length);
-            for (let i = 0; i < this.state.data.results[0].bills.length; i++) {
-                // billInfo.push(this.state.data.results[0].bills[i].title); 
-                // billInfo.push(this.state.data.results[0].bills[i].shortTitle); 
 
-                console.log("title = " + this.state.data.results[0].bills[i].short_title);
-                console.log("short title = " + this.state.data.results[0].bills[i].title);
+            for (let i = 0; i < this.state.data.results[0].bills.length; i++) {
+                // console.log("title = " + this.state.data.results[0].bills[i].short_title);
+                // console.log("short title = " + this.state.data.results[0].bills[i].title);
 
                 let data = {
                     title: this.state.data.results[0].bills[i].short_title,
-                    desc: this.state.data.results[0].bills[i].title
+                    desc: this.state.data.results[0].bills[i].title,
+                    number: this.state.data.results[0].bills[i].number
                 }
 
                 billInfo.push(data);
             } 
 
+            //setting dataRender the values, so dataRender can print onto the website
             for (let i = 0; i < billInfo.length; i++) {
-                console.log(billInfo.data.title + " " + billInfo.data.desc);
-            }
+                if (this.state.data.results[0].bills[i].title.substring(0,20) == this.state.data.results[0].bills[i].short_title.substring(0,20) ) {
+                    billInfo[i].title = "";
+                }
 
-            // console.log(billInfo.length);
-            // for (var a in billInfo) {
-            //     console.log("BillInfo: " + a); 
-            // }
+                dataRender.push(<Title idx={i+1} number={billInfo[i].number} title={billInfo[i].title} desc={billInfo[i].desc} />);
+            }
         }
 
         return (
             <div className="container">
                 <div className="row">
                     <div className = "bigText1"> 
-                    Your stop for primary resources, <br />
-                    Enter subject to search for top 20 bills:
-                    <input
-                        onChange={e => this.billsAPI(e)}
-                        type = "text"
-                        placeholder="search..."
-                        className="text-center"
-                    />
+                        Your stop for primary resources, <br />
+                        Enter subject to search for top 20 bills:
+                        <input
+                            onChange={e => this.billsAPI(e)}
+                            type = "text"
+                            placeholder="search..."
+                            className="text-center"
+                        />
+
+                        <br />
+                        <br /> 
+                    </div>
+
+                    <div className="">
+                        {dataRender}    
                     </div>
                 </div>
             </div>
